8e56ac6655f9d112cfb1d9371a18ee36cae8315a
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-09-17 23:17:12 -0700

    Create lang.js

---
+++ js/lang/lang.js
@@ -0,0 +1,64 @@
+var langArr={0:{name:'english', caption:'English', locale:'en'},1:{name:'chinese', caption:'ä¸­æ–‡', locale:'zh-cn'}};
+var defaultLang=0;
+var Lang = {    
+    Title: {
+        english: "Htmlcoin Testnet Faucet",
+        chinese: "Htmlcoinæµ‹è¯•ç½‘ç»œæµ‹è¯•ä»£å¸å‘æ”¾"
+    },
+    Faucet: {
+        english: "Faucet",
+        chinese: "æµ‹è¯•ä»£å¸å‘æ”¾"
+    },
+    entersendToAddress: {
+        english: "Please enter your QTUM testnet address to receive test tokens",
+        chinese: "è¯·è¾“å…¥ä½ çš„HTMLCOINåœ°å€æ¥æŽ¥æ”¶æµ‹è¯•ä»£å¸"
+    },
+    invalidsendToAddressPattern: {
+        english: "Invalid HTMLCOIN testnet address!",
+        chinese: "æ— æ•ˆçš„HTMLCOINåœ°å€!"
+    },
+    connError: {
+        english: "Could not connect to the server, please try again later!",
+        chinese: "æ— æ³•è¿žæŽ¥è‡³æœåŠ¡å™¨ï¼Œè¯·ç¨åŽå†è¯•ä¸€æ¬¡!"
+    },
+    dbError: {
+        english: "Could not add your request, please try again later!",
+        chinese: "æ— æ³•æ·»åŠ æ‚¨çš„è¯·æ±‚ï¼Œè¯·ç¨åŽå†è¯•ä¸€æ¬¡!"
+    },
+    addressAdded: {
+        english: "Your HTMLCOIN testnet tokens request was added to the queue, you will receive your tokens shortly.\nThank you!",
+        chinese: "æ‚¨çš„è¯·æ±‚å·²ç»è¢«æ·»åŠ è‡³é˜Ÿåˆ—ï¼Œç¨åŽå°†ä¼šæ”¶åˆ°ä»£å¸ã€‚\nè°¢è°¢!",
+    },
+    invalidResponse: {
+        english: "A server error occured, please try again later!",
+        chinese: "æœåŠ¡å™¨å›å¼‚å¸¸ï¼Œè¯·ç¨åŽå†è¯•!",
+    },
+    notEligible: {
+        english: "You can only request tokens once every 24 hours",
+        chinese: "24å°æ—¶å†…åªèƒ½è¯·æ±‚ä¸€æ¬¡ä»£å¸å“¦",
+    },
+    latestPayouts: {
+        english: "Latest Payouts",
+        chinese: "æœ€æ–°çš„æ”¯å‡º",
+    },
+    address: {
+        english: "Address",
+        chinese: "åœ°å€",
+    },
+    amount: {
+        english: "Amount",
+        chinese: "æ•°é‡",
+    } ,
+    status: {
+        english: "Status",
+        chinese: "çŠ¶æ€",
+    },
+    sent: {
+        english: "Sent",
+        chinese: "å‘é€",
+    },
+    pending: {
+        english: "Pending",
+        chinese: "ç­‰å¾…",
+    }
+}
